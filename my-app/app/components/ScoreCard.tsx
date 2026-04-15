c802b947246ffb55b1621a648f1a6e26c541d2cb
jlburks <jaren.burks1@gmail.com>
2026-04-14 21:23:18 -0500

    creating grid for scorecard

--- my-app/app/components/ScoreCard.tsx
+++ my-app/app/components/ScoreCard.tsx
@@ -2,6 +2,27 @@ export default function ScoreCard() {
     return (
         <div>
             <h1>scorecard</h1>
+            <div className="grid grid-cols-9 gap-4">
+                <div className="col-span-">hole:1</div>
+                <div className="col-span-1">hole:2</div>
+                <div className="col-span-1">hole:3</div>
+                <div className="col-span-1">hole:4</div>
+                <div className="col-span-1">hole:5</div>
+                <div className="col-span-1">hole:6</div>
+                <div className="col-span-1">hole:7</div>
+                <div className="col-span-1">hole:8</div>
+                <div className="col-span-1">hole:9</div>
+                <div className="col-span-1">hole:10</div>
+                <div className="col-span-1">hole:11</div>
+                <div className="col-span-1">hole:12</div>
+                <div className="col-span-1">hole:13</div>
+                <div className="col-span-1">hole:14</div>
+                <div className="col-span-1">hole:15</div>
+                <div className="col-span-1">hole:16</div>
+                <div className="col-span-1">hole:17</div>
+                <div className="col-span-1">hole:18</div>
+
+            </div>
         </div>
     )
 }
