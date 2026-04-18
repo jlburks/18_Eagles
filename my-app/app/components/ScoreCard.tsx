ae1c2bdc63dbda453d2b8e2eb219165e63eb0150
jlburks <jaren.burks1@gmail.com>
2026-04-17 20:59:14 -0500

    scorein sysate elements added to app

--- my-app/app/components/ScoreCard.tsx
+++ my-app/app/components/ScoreCard.tsx
@@ -1,7 +1,31 @@
+import './ScoreCard.css'
+
 export default function ScoreCard() {
     return (
-        <div>
-            <h1>scorecard</h1>
+        <div className="scoreboard">
+            <div><input className="holeScore"></input>hole1</div>
+            <div><input className="holeScore"></input>hole2</div>
+            <div><input className="holeScore"></input>hole3</div>
+            <div><input className="holeScore"></input>hole4</div>
+            <div><input className="holeScore"></input>hole5</div>
+            <div><input className="holeScore"></input>hole6</div>
+            <div><input className="holeScore"></input>hole7</div>
+            <div><input className="holeScore"></input>hole8</div>
+            <div><input className="holeScore"></input>hole9</div>
+            <div><input className="holeScore"></input>hole10</div>
+            <div><input className="holeScore"></input>hole11</div>
+            <div><input className="holeScore"></input>hole12</div>
+            <div><input className="holeScore"></input>hole13</div>
+            <div><input className="holeScore"></input>hole14</div>
+            <div><input className="holeScore"></input>hole15</div>
+            <div><input className="holeScore"></input>hole16</div>
+            <div><input className="holeScore"></input>hole17</div>
+            <div><input className="holeScore"></input>hole18</div>
+            <div>
+                <h1>Current Score: </h1>
+                 
+            </div>
+            
         </div>
     )
 }
