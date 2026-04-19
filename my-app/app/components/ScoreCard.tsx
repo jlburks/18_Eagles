4a0fb6f6cf8eeb0db70f2d814b3da50a255c08cd
jlburks <jaren.burks1@gmail.com>
2026-04-18 21:41:54 -0500

    input overflow temp fix

--- my-app/app/components/ScoreCard.tsx
+++ my-app/app/components/ScoreCard.tsx
@@ -3,24 +3,24 @@ import './ScoreCard.css'
 export default function ScoreCard() {
     return (
         <div className="scoreboard">
-            <div><input className="holeScore"></input>hole1</div>
-            <div><input className="holeScore"></input>hole2</div>
-            <div><input className="holeScore"></input>hole3</div>
-            <div><input className="holeScore"></input>hole4</div>
-            <div><input className="holeScore"></input>hole5</div>
-            <div><input className="holeScore"></input>hole6</div>
-            <div><input className="holeScore"></input>hole7</div>
-            <div><input className="holeScore"></input>hole8</div>
-            <div><input className="holeScore"></input>hole9</div>
-            <div><input className="holeScore"></input>hole10</div>
-            <div><input className="holeScore"></input>hole11</div>
-            <div><input className="holeScore"></input>hole12</div>
-            <div><input className="holeScore"></input>hole13</div>
-            <div><input className="holeScore"></input>hole14</div>
-            <div><input className="holeScore"></input>hole15</div>
-            <div><input className="holeScore"></input>hole16</div>
-            <div><input className="holeScore"></input>hole17</div>
-            <div><input className="holeScore"></input>hole18</div>
+            <div><input type="number" className="holeScore"></input>hole1</div>
+            <div><input type="number" className="holeScore"></input>hole2</div>
+            <div><input type="number" className="holeScore"></input>hole3</div>
+            <div><input type="number" className="holeScore"></input>hole4</div>
+            <div><input type="number" className="holeScore"></input>hole5</div>
+            <div><input type="number" className="holeScore"></input>hole6</div>
+            <div><input type="number" className="holeScore"></input>hole7</div>
+            <div><input type="number" className="holeScore"></input>hole8</div>
+            <div><input type="number" className="holeScore"></input>hole9</div>
+            <div><input type="number" className="holeScore"></input>hole10</div>
+            <div><input type="number" className="holeScore"></input>hole11</div>
+            <div><input type="number" className="holeScore"></input>hole12</div>
+            <div><input type="number" className="holeScore"></input>hole13</div>
+            <div><input type="number" className="holeScore"></input>hole14</div>
+            <div><input type="number" className="holeScore"></input>hole15</div>
+            <div><input type="number" className="holeScore"></input>hole16</div>
+            <div><input type="number" className="holeScore"></input>hole17</div>
+            <div><input type="number" className="holeScore"></input>hole18</div>
             <div>
                 <h1>Current Score: </h1>
 
