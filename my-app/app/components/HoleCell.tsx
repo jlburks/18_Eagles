3ec1f125bbfc8166c5dbd8071e7257e70c11d520
jlburks <jaren.burks1@gmail.com>
2026-04-23 21:39:26 -0500

    better structure implement

--- my-app/app/components/HoleCell.tsx
+++ my-app/app/components/HoleCell.tsx
@@ -1,16 +1,37 @@
 import { useState } from "react";
 
-export default function HoleCell(holeNum: number) {
+export default function HoleCell({holeNum,par,editScore}) {
     const [holeNumber, setHoleNumber] = useState(holeNum);
-    const [stokes,setStrokes] = useState()
-    const [parNumber, setParNumber] = useState(0);
+    const [strokes,setStrokes] = useState(0)
+    const [parNumber, setParNumber] = useState(par);
+
+    const assignHoleNumber = ()=> {
+        setHoleNumber(holeNumber)
+    }
+    
+
+    const getStrokes = () => {
+        if (strokes > 0) {
+            return strokes
+        } else {
+            return null
+        }
+    }
+
+    const fromPar = () => {
+        if (strokes > 0) {
+            return(strokes-parNumber)
+        }
+    }
 
     return (
         <div className="hole-cell">
             <input type="number" onChange={(e)=> {
-                setStrokes(e.target.value)
+                console.log(e)
+                setStrokes(Number(e.target.value))
+                editScore(Number(e.target.value))
             }}></input>
-            hole: {holeNumber}
+            hole: {holeNumber} par: {parNumber}
         </div>
     )
 }
