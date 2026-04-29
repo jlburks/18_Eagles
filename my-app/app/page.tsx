9e995bbf0c75c2acac130f8b0fb89dbca83a8f5b
jlburks <jaren.burks1@gmail.com>
2026-04-29 07:47:00 -0500

    app refresh start

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
-import ScoreCard from "./components/ScoreCard"
 
 export default function Home() {
   
   return (
     <div>
-      <ScoreCard></ScoreCard>
+      <h1>ScoreCard</h1>
     </div>
   );
 }
