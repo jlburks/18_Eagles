1e0a89517ca106a48de68bb93d9b7a37d2c60878
jlburks <jaren.burks1@gmail.com>
2026-05-06 08:48:16 -0500

    changes to styling

--- my-app/app/Scorecard/HoleCell.jsx
+++ my-app/app/Scorecard/HoleCell.jsx
@@ -4,13 +4,12 @@ import {useState} from 'react'
 export default function HoleCell(props) {
     
     let [prevStroke,setPrevStroke] = useState(0)
-
-
+    
     
     return (
         <div>
+            <p>hole {props.holeNumber}:</p>
             <input onChange={(e)=> {setPrevStroke(Number(e.target.value));props.updateTotal(Number(e.target.value) - prevStroke)}}></input>
-            hole#
         </div>
     )
 }
--- my-app/app/Scorecard/ScoreCard.jsx
+++ my-app/app/Scorecard/ScoreCard.jsx
@@ -22,27 +22,28 @@ export function ScoreCard() {
         <div>
 
             {/* top main stats */}
-            Total Score: {TotalScore}
-            <br></br>
+            <h1 className="text-3xl font-bold underline">Total Score: {TotalScore}</h1>
+            
             To Par: {toPar}
-            <div id="card">
-            <HoleCell className="holeCell" id="hole1" updateTotal={updateTotal}/>
-            <HoleCell className="holeCell" id="hole2" updateTotal={updateTotal}/>
-            <HoleCell className="holeCell" id="hole3" updateTotal={updateTotal}/>
-            <HoleCell className="holeCell" id="hole4" updateTotal={updateTotal}/>
-            <HoleCell className="holeCell" id="hole5" updateTotal={updateTotal}/>
-            <HoleCell className="holeCell" id="hole6" updateTotal={updateTotal}/>
-            <HoleCell className="holeCell" id="hole7" updateTotal={updateTotal}/>
-            <HoleCell className="holeCell" id="hole8" updateTotal={updateTotal}/>
-            <HoleCell className="holeCell" id="hole9" updateTotal={updateTotal}/>
-            <HoleCell className="holeCell" id="hole10" updateTotal={updateTotal}/>
-            <HoleCell className="holeCell" id="hole12" updateTotal={updateTotal}/>
-            <HoleCell className="holeCell" id="hole13" updateTotal={updateTotal}/>
-            <HoleCell className="holeCell" id="hole14" updateTotal={updateTotal}/>
-            <HoleCell className="holeCell" id="hole15" updateTotal={updateTotal}/>
-            <HoleCell className="holeCell" id="hole16" updateTotal={updateTotal}/>
-            <HoleCell className="holeCell" id="hole17" updateTotal={updateTotal}/>
-            <HoleCell className="holeCell" id="hole18" updateTotal={updateTotal}/>
+            <div id="card" className="grid grid-cols-3 gap-4">
+            <HoleCell className="holeCell" id="hole1" updateTotal={updateTotal} holeNumber={1}/> 
+            <HoleCell className="holeCell" id="hole2" updateTotal={updateTotal} holeNumber={2}/> 
+            <HoleCell className="holeCell" id="hole3" updateTotal={updateTotal} holeNumber={3}/> 
+            <HoleCell className="holeCell" id="hole4" updateTotal={updateTotal} holeNumber={4}/> 
+            <HoleCell className="holeCell" id="hole5" updateTotal={updateTotal} holeNumber={5}/> 
+            <HoleCell className="holeCell" id="hole6" updateTotal={updateTotal} holeNumber={6}/> 
+            <HoleCell className="holeCell" id="hole7" updateTotal={updateTotal} holeNumber={7}/> 
+            <HoleCell className="holeCell" id="hole8" updateTotal={updateTotal} holeNumber={8}/> 
+            <HoleCell className="holeCell" id="hole9" updateTotal={updateTotal} holeNumber={9}/> 
+            <HoleCell className="holeCell" id="hole10" updateTotal={updateTotal} holeNumber={10}/> 
+            <HoleCell className="holeCell" id="hole12" updateTotal={updateTotal} holeNumber={11}/> 
+            <HoleCell className="holeCell" id="hole13" updateTotal={updateTotal} holeNumber={12}/>
+            <HoleCell className="holeCell" id="hole14" updateTotal={updateTotal} holeNumber={13}/>
+            <HoleCell className="holeCell" id="hole15" updateTotal={updateTotal} holeNumber={14}/>
+            <HoleCell className="holeCell" id="hole16" updateTotal={updateTotal} holeNumber={15}/>
+            <HoleCell className="holeCell" id="hole17" updateTotal={updateTotal} holeNumber={16}/>
+            <HoleCell className="holeCell" id="hole18" updateTotal={updateTotal} holeNumber={17}/>
+            <HoleCell className="holeCell" id="hole18" updateTotal={updateTotal} holeNumber={18}/>
             </div>
         </div>
     )
