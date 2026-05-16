ed55236f5acb837ebea251e0c48dfbae34ce6cc5
jlburks <jaren.burks1@gmail.com>
2026-05-15 20:42:58 -0500

    page updated

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,21 +1,20 @@
 import { TurborepoAccessTraceResult } from "next/dist/build/turborepo-access-trace";
 import { Holtwood_One_SC } from "next/font/google";
 import Image from "next/image";
-import {ScoreCard} from './Scorecard/ScoreCard'
 
 export default function Home() {
 
  
   
   return ( 
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      {/* app name */}
-      <h1>18 Eagles</h1>
-      {/* course name */}
-      <h2>Course Name</h2>
-      <ScoreCard />
-      
-          
+    <div>
+      <h1>18 Birdies</h1>
+      <h4>87</h4>
+      <h6>88</h6>
+      <h2>The Wilderness</h2>
+      <section>
+        
+      </section>
     </div>
   );
 }
